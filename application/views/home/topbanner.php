f82aa3c1fa0b6640a07af0d077f00f39eae25570
Risatya <rizk_satya@yahoo.com>
2014-09-05 15:43:43 +0700

    commit #5 05-09-2014 : Edit responsive site

--- application/views/home/topbanner.php
+++ application/views/home/topbanner.php
@@ -1,4 +1,4 @@
 		<div id="content_wrapper">
 			<div id="left_wrapper">
 				<div id="top_banner"><?php echo anchor(($banner['top1']['url'] == null ? "#" : "http://".$banner['top1']['url']),"<img src='".base_url()."file/img/banner/".$banner['top1']['pic']."'/>"); ?></div>
-				<div id="maskot"></div>
+				
